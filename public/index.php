6cee25d5038912e5930e3b64136c635acdc9d776
Lloyd <leblanc71u@etu.univ-lorraine.fr>
2022-10-23 18:49:11 +0200

    modification interface
    Début de l'implémentation de l'interface avec twig , découverte des parametres
    début de la fonction draw qui devra allez dans CardController

--- public/index.php
+++ public/index.php
@@ -6,7 +6,14 @@ use Slim\Factory\AppFactory;
 use Slim\Views\TwigMiddleware;
 use Slim\Views\Twig;
 
+if (!isset($_COOKIE['card'])) {
+    $arrCard = [];
+    setcookie("card", json_encode($arrCard), time()+60*60*24*100, "/");
+}
+
+
 require __DIR__ . '/../vendor/autoload.php';
+
 
 $container = require_once __DIR__ . '/../bootstrap.php';
 
@@ -21,18 +28,21 @@ $app->add(TwigMiddleware::create($app,$twig));
 
 $app->get('/', function (Request $request, Response $response, $args) {
     $view = Twig::fromRequest($request);
-    return $view->render($response, 'hello.twig', [
+    return $view->render($response, 'app.twig', [
     ]);
 });
 
+$app->post('/', \App\draw::class . ':draw');
+$app->post('/combine', \App\draw::class . ':test');
+
 $app->get('/test/{name}', function (Request $request, Response $response, $args) {
     $view = Twig::fromRequest($request);
-    return $view->render($response, 'hello.twig', [
+    return $view->render($response, 'app.twig', [
         'name' => $args['name']
     ]);
 });
 
-
+$app->get('/card', \App\CardController::class . ':test');
 
 $app->get('/users', \App\UserController::class . ':test');
 
--- src/draw.php
+++ src/draw.php
@@ -5,21 +5,23 @@ use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Slim\Views\Twig;
 
-class UserController
+class draw
 {
   private $view;
 
-  public function __construct(Twig $view, UserService $userService)
+  public function __construct(Twig $view)
   {
     $this->view = $view;
-    $this->userService = $userService;
   }
 
   public function test(ServerRequestInterface $request, ResponseInterface $response, array $args): ResponseInterface
   {
-    $user = $this->userService->signUp('test');
+    $data = json_decode($_COOKIE['card'],true);
+    array_push($data,$_POST["cardId"]);
+    setcookie('card',json_encode($data) , time()+3600, '/');
     return $this->view->render($response, 'app.twig', [
-      'name' => 'me',
+        'value' => $_POST["cardId"],
+        'tabCard' => $data,
     ]);
     return $response;
   }
