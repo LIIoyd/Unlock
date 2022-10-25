fe58d9bbfeb1f3b511999afd51bfa1501a68d480
Lhiikan <antoine.chevaleyre6@etu.univ-lorraine.fr>
2022-10-25 09:32:02 +0200

    sql insert card

---
+++ src/sql/card_202210250930.sql
@@ -0,0 +1,24 @@
+INSERT INTO `Unlock`.card (`number`,`type`,img,side,imgDos) VALUES
+	 ('4','bleu','deck_cartes_unlock/4/4.png',1,'deck_cartes_unlock/4/4dos.png'),
+	 ('15','gris','deck_cartes_unlock/15/15.png',0,'deck_cartes_unlock/15/15dos.png'),
+	 ('21','jaune','deck_cartes_unlock/21/21.png',1,'deck_cartes_unlock/21/21dos.png'),
+	 ('22','bleu','deck_cartes_unlock/22/22.png',1,'deck_cartes_unlock/22/22dos.png'),
+	 ('32','rouge','deck_cartes_unlock/32/32.png',0,'deck_cartes_unlock/32/32dos.png'),
+	 ('35','bleu','deck_cartes_unlock/35/35.png',0,'deck_cartes_unlock/35/35dos.png'),
+	 ('42','bleu','deck_cartes_unlock/42/42.png',1,'deck_cartes_unlock/42/42dos.png'),
+	 ('47','gris','deck_cartes_unlock/47/47.png',0,'deck_cartes_unlock/47/47dos.png'),
+	 ('50','bleu','deck_cartes_unlock/50/50.png',0,'deck_cartes_unlock/50/50dos.png'),
+	 ('60','jaune','deck_cartes_unlock/60/60.png',0,'deck_cartes_unlock/60/60dos.png');
+INSERT INTO `Unlock`.card (`number`,`type`,img,side,imgDos) VALUES
+	 ('63','rouge','deck_cartes_unlock/63/63.png',0,'deck_cartes_unlock/63/63dos.png'),
+	 ('67','gris','deck_cartes_unlock/67/67.png',0,'deck_cartes_unlock/67/67dos.png'),
+	 ('73','jaune','deck_cartes_unlock/73/73.png',0,'deck_cartes_unlock/73/73dos.png'),
+	 ('79','gris','deck_cartes_unlock/79/79.png',0,'deck_cartes_unlock/79/79dos.png'),
+	 ('80','bleu','deck_cartes_unlock/80/80.png',0,'deck_cartes_unlock/80/80dos.png'),
+	 ('85','vert','deck_cartes_unlock/85/85.png',0,'deck_cartes_unlock/85/85dos.png'),
+	 ('C','gris','deck_cartes_unlock/C/C.png',0,'deck_cartes_unlock/C/Cdos.png'),
+	 ('K','gris','deck_cartes_unlock/K/K.png',0,'deck_cartes_unlock/K/Kdos.png'),
+	 ('M','gris','deck_cartes_unlock/M/M.png',0,'deck_cartes_unlock/M/Mdos.png'),
+	 ('chambre','gris','deck_cartes_unlock/chambre_hotel.png',1,'deck_cartes_unlock/chambre_hotel.png');
+INSERT INTO `Unlock`.card (`number`,`type`,img,side,imgDos) VALUES
+	 ('elite','gris','deck_cartes_unlock/l_elite.png',1,'deck_cartes_unlock/l_elite.png');
